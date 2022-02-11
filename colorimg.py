e9212710423cf15d64d6b3808922bd46406285b9
Chinmay1292 <chinmaychougule1212@gmail.com>
2022-02-12 00:06:10 +0530

    colour

--- colorimg.py
+++ colorimg.py
@@ -2,4 +2,10 @@ import cv2
 image=cv2.imread("Messi.jpg")
 
 B,G,R=cv2.split(image)
-print(B,shape)
+print(B.shape)
+cv2.imshow("original",image)
+cv2.imshow("Red_Image",R)
+cv2.imshow("Blue_Image",B)
+cv2.imshow("Green_Image",G)
+cv2.waitKey()
+cv2.destroyAllWindows()
