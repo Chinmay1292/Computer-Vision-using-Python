a8e52a7d172367650650b9892ffdf96e5d69db24
Chinmay1292 <chinmaychougule1212@gmail.com>
2022-02-27 00:37:54 +0530

    number plate

---
+++ numberplate.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img=cv2.imread("car.jpg")
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+filter=cv2.bilateralFilter(gray,11,15,15)
+edge=cv2.Canny(filter,170,200)
+contor,herf =cv2.findContours(edge,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+ctn=sorted(contor,key=cv2.contourArea,reverse=True)
+for c in ctn:
+    peri=cv2.arcLength(c,True)
+    epsilon = 0.018*peri
+    approx=cv2.approxPolyDP(c,epsilon,True)
