b42ed50c541631654d445f23b8b46b7b36eda9bd
Chinmay1292 <chinmaychougule1212@gmail.com>
2022-02-27 01:10:51 +0530

    number plate

--- numberplate.py
+++ numberplate.py
@@ -1,19 +1,10 @@
 import cv2
+import pytesseract
 import numpy as np
 
-img=cv2.imread("car.jpg")
-gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-filter=cv2.bilateralFilter(gray,11,15,15)
-edge=cv2.Canny(filter,170,200)
-contor,herf =cv2.findContours(edge,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-ctn=sorted(contor,key=cv2.contourArea,reverse=True)
-for c in ctn:
-    peri=cv2.arcLength(c,True)
-    epsilon = 0.018*peri
-    approx=cv2.approxPolyDP(c,epsilon,True)
-    if len(approx)==4:
-        final=cv2.drawContours(img,[approx],-1,(255,0,0),3)
-        break
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-cv2.imshow("Number Plate Detected",img)
-cv2.waitKey(0)
+img = cv2.imread("car.jpg")
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+gray = cv2.bilateralFilter(gray, 11,17,17)
